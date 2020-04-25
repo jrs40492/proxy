f459cbf7b4d281b55ccf9afe3e6560410c692e54
Jacob Swanson <jacobswanson@Jacobs-MacBook-Pro.local>
2020-04-25 01:37:35 -0400

    Fix typo

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 docker network create nginx-proxy
-docker netwerk create db_network
+docker network create db_network
 docker network create --driver bridge ufsports_net
 
 docker-compose up -d
